b612bd76439b3a6a0c6e4dfa5ea741a78ec59f6d
An-GG <ankush.girotra@gmail.com>
2022-08-31 09:27:07 -0500

    stdin

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env ts-node
 import getStdin from "get-stdin";
-
 async function main() {
     let input = await getStdin();
-    console.log("h=i  "+input);
+    console.log("h=i  " + input);
 }
-
 main();
+//# sourceMappingURL=index.js.map
